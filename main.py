2cb152e0b8a69f4f95490e471d11023cdd8013e8
nurriol2 <nurriol2@illinois.edu>
2020-07-10 00:35:14 -0500

    Throw an InvalidItemIDError
    Testing exception handling with an example.

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from tradeable import TradeableItem
+
+def main():
+
+    iphone = TradeableItem("iphone")
+    print(iphone.id)
+
+    return
+
+if __name__=="__main__":
+    main()
