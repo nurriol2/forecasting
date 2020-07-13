60d83c3b706ab00ac5edf1b3a0ed4404379ef9e9
nurriol2 <nurriol2@illinois.edu>
2020-07-13 01:19:30 -0500

    Include tests for `table` initialization

---
+++ tests.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import unittest
+from tradeable_item import TradeableItem
+
+class Test(unittest.TestCase):
+    def test_collect_volume_time_series(self):
+
+        dagger = TradeableItem("Rune Dagger")
+        
+        colnames = ["Close", "Average", "Volume"]
+
+        for colname in colnames:
+            assert dagger.table[colname].size == 180
+
+        return
