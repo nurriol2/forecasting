b19c631b98e835c5d6ac2655d0007e6f47632a5f
nurriol2 <nurriol2@illinois.edu>
2020-07-19 00:07:22 -0500

    Update documentation for all defined exceptions

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 class InvalidItemIDError(Exception):
-    """Raised when `TradeableItem.name` is not found in `items`.
-
-    Args:
-        Error (InvalidItemID): When a `TradeableItem` is created, a `name` variable is set. Then, `name` 
-        is used to search a list of dictionaries mapping the item id to the item name. If the search yields
-        no result, `TradeableItem.id` == `None`.
+    """Raised when `name` is not found in `item_ids.py`
     """
 
     def __init__(self, message="The entered name did not yield a valid item ID. Item id:  None"):
         self.message = message
+        super().__init__(self.message)
+
+class MismatchedSeriesSizeError(Exception):
+    """Raised if series is initialized with time series of different lengths
+    """
+    def __init__(self, expected_npts, candidate, seriesID):
+        self.expected_npts = expected_npts
+        self.candidate = candidate
+        self.seriesID = seriesID
+        self.message = "One or more columns has a different size than expected.\nExpecting {} entries.\nGot {} entries in series {}.".format(self.expected_npts, self.candidate, self.seriesID)
         super().__init__(self.message)
