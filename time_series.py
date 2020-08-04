968ad97c443b1e28da9327e1b596b6de881f7389
nurriol2 <nurriol2@illinois.edu>
2020-08-04 00:07:41 -0500

    Format timestamps from epoch or ISO patterns
    There is only a small time period where the timestamps for VOLUME
    and PRICE series have concurrent timestamps. So, assuming that
    timestamps from one can be used for another is only true for that
    time period.
    
    To make data collection robust to the the collection time (i.e. in
    or out of the concurrent window) it's better to collect the *both*
    sets of timestamps and merge wherever the sets are concurrent.
    
    The tradeoff for robustness is that there will unavoidably be missing
    data after merging. When the table is processed, the missing data
    needs to be formatted.

--- time_series.py
+++ time_series.py
@@ -2,6 +2,8 @@
 
 import re
 import pandas as pd
+from datetime import datetime
+from exceptions import TimestampError
 
 class TimeSeries:
 
@@ -13,8 +15,10 @@ class TimeSeries:
             timestamps (list): The timestamps of the dataset. Agnostic to data type.
             signal (list): Signal being tracked via time series. Agnostic to data type.
         """
-        self.timestamps = timestamps 
+        self.timestamps = timestamps
         self.signal = signal
+        
+        self.timestamps = self._format_timestamps() 
         return 
 
     @classmethod
@@ -34,7 +38,29 @@ class TimeSeries:
         self.signal = list(data.values())
         return TimeSeries(self.timestamps, self.signal)
 
-    def _format_timestamps(self, timestamps):
+    def _format_timestamps(self):
+        """Convert timestamps into datetime objects
+
+        Raises:
+            TimestampError: If timestamps do not match epoch or iso pattern
+
+        Returns:
+            list: list of timestamps as datetime objects
+        """
         epoch_pattern = "\d{13}"
-        
-        return 
+        iso_pattern = "\d{4}/\d{2}/\d{2}"
+
+        formatted_timestamps = []
+        if re.match(epoch_pattern, self.timestamps[0]):
+            for ts in self.timestamps:
+                fmt_ts = pd.to_datetime(int(ts), unit="ms").strftime("%Y/%m/%d")
+                formatted_timestamps.append(fmt_ts)
+        elif re.match(iso_pattern, self.timestamps[0]):
+            for ts in self.timestamps:
+                y, m, d = ts.split("/")
+                fmt_ts = datetime(int(y), int(m), int(d)).strftime("%Y/%m/%d")
+                formatted_timestamps.append(fmt_ts)
+        else:
+            raise TimestampError
+
+        return formatted_timestamps
