bea082002fa76cb0147b14aa6e4f72d935bf211b
nurriol2 <nurriol2@illinois.edu>
2020-08-08 21:59:14 -0500

    Demo bar graph plotting

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
 from tradeable_item import TradeableItem
+import matplotlib.pyplot as plt
+import pandas as pd
 
 def main():
+    
+    #red chinchompas item
+    chins = TradeableItem("Red chinchompa")
+    table = chins.table
 
-    magicLogs = TradeableItem("Magic Logs")
-    t = magicLogs.table
-    print(t.head())
-    print(t.shape)
+    chins.plot_bar_graph("Item Timestamps", ["Volume"], "Volume of Units Moved per Day", ylabel="# of Red Chinchompas", save_plot=False)
+    
+
     return
 
 if __name__=="__main__":
