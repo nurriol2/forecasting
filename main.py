e505e18c58f96e95ac885a0f5f98a08c9b604821
nurriol2 <nurriol2@illinois.edu>
2020-08-11 00:21:02 -0500

    Normalize then append data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
-from tradeable_item import TradeableItem
-import matplotlib.pyplot as plt
+import math
 import pandas as pd
+import matplotlib.pyplot as plt
+from tradeable_item import TradeableItem
+from sklearn.preprocessing import MinMaxScaler
 
 def main():
     
@@ -10,9 +12,17 @@ def main():
     chins = TradeableItem("Red chinchompa")
     table = chins.table
 
-    chins.plot_bar_graph("Item Timestamps", ["Volume"], "Volume of Units Moved per Day", ylabel="# of Red Chinchompas", save_plot=False, verbose=True)
-    chins.plot_time_series("Item Timestamps", ["Close", "Average"], "Red Chins Closing and Average Price", ylabel="Price (GP)", save_plot=False, verbose=True)
-    chins.correlation_matrix(["Close", "Average", "Volume"], "Correlation Matrix", save=False, verbose=True)
+    #chins.plot_bar_graph("Item Timestamps", ["Volume"], "Vol. of Red Chins Moved per Day", ylabel="# of Red Chins Sold", save_plot=False, verbose=True)
+    #chins.plot_time_series("Item Timestamps", ["Close", "Average"], "Red Chins Closing and Average Price", ylabel="Price (GP)", save_plot=False, verbose=True)
+    #chins.correlation_matrix(["Close", "Average", "Volume"], " Red Chins Correlation Matrix", save=False, verbose=True)
+    
+    #add normalized data to the table
+    cols = ["Close", "Average", "Volume"]
+    for c in cols:
+        scaler = MinMaxScaler()
+        data = table[c].to_numpy().reshape(-1,1)
+        norm_data = scaler.fit_transform(data).flatten()
+        table["Normalized {}".format(c)] = norm_data
     
     return
 
